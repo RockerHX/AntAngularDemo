22b912108fad538e3195482fa00810e52b59c6f9
RockerHX <rockerhx@gmail.com>
2018-01-31 21:32:12 +0800

    Update[Component]: Layout
    采用动态显示的方式来展示菜单。

--- src/app/nzdemo-layout/nzdemo-layout.component.ts
+++ src/app/nzdemo-layout/nzdemo-layout.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MENUS } from '../models/menu';
+
 @Component({
   selector: 'app-nzdemo-layout',
   templateUrl: './nzdemo-layout.component.html',
   styleUrls: ['./nzdemo-layout.component.css']
 })
 export class NzdemoLayoutComponent implements OnInit {
+
+  menus = MENUS;
 
   constructor() { }
 
